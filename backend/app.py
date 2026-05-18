6e4128011adc75017c26fef45532db933d15a8b6
Amoghzzz <amogh.chowdhary@gmail.com>
2026-05-18 20:24:24 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+genai.configure(
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+model = genai.GenerativeModel("gemini-2.5-pro")
+
+app = FastAPI()
+
+class Query(BaseModel):
+    question:str
+
+@app.post("/ask")
+
+def ask(query:Query):
+
+    prompt=f"""
+    You are a regulatory compliance AI expert.
+
+    Answer accurately.
+
+    Question:
+    {query.question}
+    """
+
+    response=model.generate_content(prompt)
+
+    return {
+        "answer":response.text
+    }
