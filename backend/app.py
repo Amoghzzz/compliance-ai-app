c5de84f8fbc2c2526f23718a1ede1c706e1574f7
Amoghzzz <amogh.chowdhary@gmail.com>
2026-05-18 20:25:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ genai.configure(
     api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-2.5-pro")
+model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 app = FastAPI()
 
